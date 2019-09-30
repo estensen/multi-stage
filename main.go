2e49156fb6a383edf58dab10d7248e0d2f0af8dd
Håvard Anda Estensen <haavard.ae@gmail.com>
2019-09-30 09:36:13 +0200

    Add logging middleware

--- main.go
+++ main.go
@@ -7,13 +7,20 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	log.Infof("uri: %s", r.RequestURI)
-	fmt.Fprint(w, "Hello Go")
+func main() {
+	helloHandler := http.HandlerFunc(hello)
+	http.Handle("/", logging(helloHandler))
+	log.Info("Server running on port 8000")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
-func main() {
-	http.HandleFunc("/", helloHandler)
-	fmt.Println("Server running on port 8000")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+func logging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func( w http.ResponseWriter, r *http.Request) {
+		log.Infof("uri: %s", r.RequestURI)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func hello(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprint(w, "Hello Go")
 }
